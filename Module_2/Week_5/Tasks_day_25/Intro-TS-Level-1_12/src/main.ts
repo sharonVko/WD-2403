4164506b8c310cafeb57f02e2d1336e93ee30a8f
Supercoder <supercoder@AirvonSpercoder.fritz.box>
2024-09-27 15:52:09 +0200

    added task ts-intro-level 1 day 25

---
+++ Module_2/Week_5/Tasks_day_25/Intro-TS-Level-1_12/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   <a href='./src/tsBasics/basic.html'>Basic Ts</a>
+  </div>
+`
+
+
---
+++ Module_2/Week_5/Tasks_day_25/Intro-TS-Level-1_12/src/tsBasics/basic.ts
@@ -0,0 +1,34 @@
+console.log("hello");
+
+let dogName = 'Leon';
+
+console.log(dogName);
+
+dogName = "elf";
+
+console.log(dogName);
+
+
+let horseName = "my little pony"
+console.log(horseName); /* returns my little pony */
+ 
+horseName = "fury";
+console.log(horseName); /* returns fury */
+
+let dogAge = 8;  
+console.log(dogAge); /* returns 8 */
+
+let vetCheck:boolean = true; 
+console.log(vetCheck); /* returns true */
+
+let seventeen;
+const sum = seventeen + 44;
+console.log(sum); /* returns NaN */
+
+
+
+
+
+
+
+ 
