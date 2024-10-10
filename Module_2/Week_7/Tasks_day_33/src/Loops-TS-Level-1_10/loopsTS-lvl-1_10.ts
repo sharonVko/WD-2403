f7a5dee163040b6f432b87dc76f384f01da07d14
Supercoder <supercoder@AirvonSpercoder.fritz.box>
2024-10-11 00:37:49 +0200

    added another task  with loop for day 33

---
+++ Module_2/Week_7/Tasks_day_33/src/Loops-TS-Level-1_10/loopsTS-lvl-1_10.ts
@@ -0,0 +1,11 @@
+console.log("practice loops");
+
+const getHighestNumber = (numArr:number []) => { /* getHighestNumber function expecting the numArr array as parameter */
+    let highestNum:number = numArr[0];
+    for(let digit:number = 1; digit < numArr.length; digit++){ /* loop starting at index 1, until end of array */
+        if(numArr > highestNum){
+
+        }
+         /* not sure how to continue here.. */
+    }
+} 
