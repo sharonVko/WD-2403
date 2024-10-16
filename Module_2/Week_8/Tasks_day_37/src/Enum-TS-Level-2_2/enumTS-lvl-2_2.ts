eca1f97dabd09e65f26c613eb366e959016cde80
Supercoder <supercoder@AirvonSpercoder.fritz.box>
2024-10-16 17:59:22 +0200

    added another enum tasks of day 37

--- Module_2/Week_8/Tasks_day_37/src/Enum-TS-Level-2_2/enumTS-lvl-2_2.ts
+++ Module_2/Week_8/Tasks_day_37/src/Enum-TS-Level-2_2/enumTS-lvl-2_2.ts
@@ -1,4 +1,8 @@
 console.log("practice enums, loop");
+//Du sollst eine Farbauswahl mit Hilfe von Buttons in einem Online-Shop erstellen
+
+//- Definiere ein Enum ClothingColor mit dem Farbnamen und der Entsprechung in Hex-Werten
+//- Es gibt diese Farben: Gelb, Orange, Pink, Blau, Lila, und Grau
 
 enum ClothingColor {
 
@@ -11,4 +15,13 @@ enum ClothingColor {
 };
 console.log(ClothingColor); /* console returns: {Gelb: '#FFFF00', Orange: '#FFA500', Pink: '#FFC0CB', Blau: '#0000FF', Lila: 'A020F0', …}Blau: "#0000FF"Gelb: "#FFFF00"Grau: "#BEBEBE"Lila: "A020F0"Orange: "#FFA500"Pink: "#FFC0CB"[[Prototype]]: Object */
 
-/* const allColors */
+//Lege dir ein Array mit allen Farben an: allColors: Color[]
+const allColors: ClothingColor [] = [
+    ClothingColor.Blau,
+    ClothingColor.Gelb,
+    ClothingColor.Grau,
+    ClothingColor.Lila,
+    ClothingColor.Orange,
+    ClothingColor.Pink
+];
+console.log(allColors); /*  console returns: (6) ['#0000FF', '#FFFF00', '#BEBEBE', 'A020F0', '#FFA500', '#FFC0CB'] */
