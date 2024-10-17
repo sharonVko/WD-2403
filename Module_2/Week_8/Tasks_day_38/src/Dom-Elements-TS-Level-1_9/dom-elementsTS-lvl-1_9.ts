ceccf6536969ac6c1400965bcce8b9ccd746c0a4
Supercoder <supercoder@AirvonSpercoder.fritz.box>
2024-10-17 21:48:58 +0200

    finished DOM tasks level 1 of day 38

--- Module_2/Week_8/Tasks_day_38/src/Dom-Elements-TS-Level-1_9/dom-elementsTS-lvl-1_9.ts
+++ Module_2/Week_8/Tasks_day_38/src/Dom-Elements-TS-Level-1_9/dom-elementsTS-lvl-1_9.ts
@@ -6,6 +6,30 @@ console.log("practice addEventListener(), selectedIndex and remove()");
 
 //  - [addEventListener()]für den Klick auf den Button
 
+
+
 // - [selectedIndex()]um dir das ausgewählte Element aus dem Select zu holen
 
 //  - [remove()] um über den Index das Element zu löschen
+
+const farbeAuswahlen = document.getElementById('farbeAuswahlen') as HTMLSelectElement;
+console.log(farbeAuswahlen);
+
+const colorOptions = document.getElementsByTagName('option') as HTMLCollection;
+console.log(colorOptions);
+/* console.log(colorOptions[3]); */
+
+
+const removeBtn = document.querySelector('#button') as HTMLButtonElement;
+console.log(removeBtn);
+
+removeBtn?.addEventListener('click', (event) => {
+    event.preventDefault() // to stop default setting of button (within the form) which keeps sending the form over and over again
+    console.log("test");
+
+    const selectColor = farbeAuswahlen.selectedIndex; /* getting index of selected option */
+    
+    if(selectColor !== -1){
+        farbeAuswahlen.options[selectColor].remove(); /* if an option was selected, remove it ! */
+    }
+});
