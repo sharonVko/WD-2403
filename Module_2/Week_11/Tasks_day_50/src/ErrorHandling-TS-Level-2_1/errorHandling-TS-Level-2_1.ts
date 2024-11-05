c1331d1d5ffd3f76ea6002e56870c9814dc37b57
Sharon <shar1.v.kori@gmail.com>
2024-11-06 00:54:21 +0100

    finished taskss of day 50 error handling

--- Module_2/Week_11/Tasks_day_50/src/ErrorHandling-TS-Level-2_1/errorHandling-TS-Level-2_1.ts
+++ Module_2/Week_11/Tasks_day_50/src/ErrorHandling-TS-Level-2_1/errorHandling-TS-Level-2_1.ts
@@ -1,22 +1,34 @@
 console.log("test");
 
+//- Schreibe eine Funktion generateLottoNumber, die eine Lottozahl generiert.
 const generateLottoNumber = () => {
-    try{
-
-    const lottoNumber: number = Math.round(Math.random() * 100)
-    
+    //generiere dort eine Zufallszahl zwischen 1 und 100 mit Math.random().
+    const lottoNumber: number = Math.round(Math.random() * 100) +1;
+    //Wenn die random erstellte Zahl größer als 49 ist, soll ein Fehler ausgelöst werden.
     if(lottoNumber > 49){
         throw new Error("number is bigger than 49")
     } else {
         /* console.log(`Your number is ${lottoNumber}`); */
-        return lottoNumber
+        //Ansonsten wird die Zahl als Rückgabewert zurückgegeben
+        return lottoNumber;
     }
+};
 
-    }catch (err) {
-        console.log("Sorry, the number was too big");
-        
+//Verwende einen try-catch-Block, um Fehler abzufangen und zu behandeln.
+//Schreib die generierte Lottozahl in ein Array lottoResults: number[], wenn kein Fehler aufgetreten ist.
+let lottoNumResults: number[] =  [];
+
+while(lottoNumResults.length < 7) {
+try{
+    let number = generateLottoNumber();
+    if(!lottoNumResults.includes(number)) { //Es dürfen keine doppelten Zahlen in lottoResults aufgenommen werden
+        lottoNumResults.push(number); // if not yet existent inside array, push that number
     }
-    
-}
+}catch (err) {
+    console.log("Sorry, the number was too big");
+}  ; 
+};
 
-generateLottoNumber()
+
+//Lass dir am Ende die Lottozahlenreihe auf der Konsole ausgeben
+console.log(`And finally, ...your numbers are: ${lottoNumResults}` ); // console returns: And finally, ...your numbers are: 2,16,45,46,47,21,19
