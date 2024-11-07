eaf5169b1124a4d54877b6dd091299fd719f379b
Sharon <shar1.v.kori@gmail.com>
2024-11-07 18:16:35 +0100

    started level 2_1  of day 52 promises

--- Module_2/Week_11/Tasks_day_52/src/Promise-TS-Level-2_1/promise-TS-Level-2_1.ts
+++ Module_2/Week_11/Tasks_day_52/src/Promise-TS-Level-2_1/promise-TS-Level-2_1.ts
@@ -1,5 +1,14 @@
 // (a.k.a number-methods.ts)
 
-const randomNum:Promise<number> = new Promise((resolve, reject) => {
-    
-})
+//Erstelle ein Promise vom Typ number[]
+//Lass dir mit Math.random drei zufällige Werte zwischen 1 und 130 generieren
+const randomNumbers:Promise<number []> = new Promise((resolve, reject) => {
+    const num1:number = Math.floor(Math.random() * 130) + 1; 
+    const num2:number = Math.floor(Math.random() * 130) + 1; 
+    const num3:number = Math.floor(Math.random() * 130) + 1; 
+    if(num1 <100 ||num2 < 100||num3 < 100) {
+        resolve([num1, num2, num3]); 
+     } else {
+                reject('Sorry, at least one number is too big')
+            } 
+});
