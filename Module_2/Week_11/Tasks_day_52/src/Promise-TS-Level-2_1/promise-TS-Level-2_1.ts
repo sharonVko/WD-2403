d6c42ee0567d261357a84dc56c785a2b5ec066ef
Sharon <shar1.v.kori@gmail.com>
2024-11-07 23:59:10 +0100

    finished level 2_1  of day 52 promises

--- Module_2/Week_11/Tasks_day_52/src/Promise-TS-Level-2_1/promise-TS-Level-2_1.ts
+++ Module_2/Week_11/Tasks_day_52/src/Promise-TS-Level-2_1/promise-TS-Level-2_1.ts
@@ -7,8 +7,36 @@ const randomNumbers:Promise<number []> = new Promise((resolve, reject) => {
     const num2:number = Math.floor(Math.random() * 130) + 1; 
     const num3:number = Math.floor(Math.random() * 130) + 1; 
     if(num1 <100 ||num2 < 100||num3 < 100) {
-        resolve([num1, num2, num3]); 
+        resolve([num1, num2, num3]); //Schreibe andernfalls die drei Werte in ein Array und resolve das Array
      } else {
-                reject('Sorry, at least one number is too big')
+                reject('Sorry, at least one number is too big') //ist einer der Werte größer als 100, rejecte das komplette Promise mit einem Fehlertext
             } 
 });
+//- Hänge dich an das Promise. Es werden fünf then-Blöcke aneinandergehängt (Chaining)
+randomNumbers.then(numbers => {
+    //Im ersten then-Block soll auf jeden Wert 7 aufaddiert werden
+    console.log('Round 1: adding 7 to each number: ', numbers);
+    return numbers.map(num => num + 7);
+    //Im zweiten then-Block soll jeder Wert mit 2 multipliziert werden
+}).then(numbers => {
+    console.log('Round 2: multiply* them by 2:  ', numbers);
+    return numbers.map(num => num * 2)
+    // //Im dritten then-Block soll von jedem Wert 1 abgezogen werden
+}).then(numbers => {
+    console.log('Round 3: subtract them by 1:  ', numbers);
+    return numbers.map(num => num - 1);
+    //Im vierten then-Block soll für jeden Wert Modulo 2 ermittelt werden
+}).then(numbers => {
+    console.log('Round 4: find their %modulo 2 : ', numbers);
+    return numbers.map(num => num % 2);
+    //Im fünften then-Block sollen die Werte auf die Konsole geloggt werden
+}).then(numbers => {
+    console.log("Round 5: show 'em all", numbers);
+    //Gib im catch-Block den Fehler auf der Konsole aus
+}).catch((error) => {
+    console.error(error);
+    //Gib im finally-Block “Finished” auf der Konsole aus
+}).finally(() => {
+    console.log('Finished');
+});
+
